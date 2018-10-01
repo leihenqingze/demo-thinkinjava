29b5774a3dcba3b5ba5c7cb7c78496724d28e36a
leihenqingze <leihenqingze@sina.com>
2018-10-01 23:25:13 +0800

    访问权限控制

---
+++ src/main/java/org/lhqz/demo/thinkinginjava/util/PrintTest.java
@@ -0,0 +1,12 @@
+package org.lhqz.demo.thinkinginjava.util;
+
+import static org.lhqz.demo.thinkinginjava.util.Print.*;
+
+public class PrintTest {
+    public static void main(String[] args) {
+        print("Available from now on!");
+        print(100);
+        print(100L);
+        print(3.14159);
+    }
+}
